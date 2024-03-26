3c1b59e8fc8dd0cae162a655d64c4e92aac021a7
brahma shy <875101222@qq.com>
2024-03-26 19:41:28 +0800

    新增 starter 启动插件模块

---
+++ brahma-spring-boot-starter/src/main/java/com/brahma/BrahmaApplication.java
@@ -0,0 +1,11 @@
+package com.brahma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrahmaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrahmaApplication.class, args);
+    }
+}
