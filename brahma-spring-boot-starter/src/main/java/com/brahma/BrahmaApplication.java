a1fd61a820f32cd68541cf8a7db02244ed4b6327
brahma shy <875101222@qq.com>
2024-04-07 20:28:42 +0800

    新增 brahma-security-auth 模块 实现单点登录功能

--- brahma-spring-boot-starter/src/main/java/com/brahma/BrahmaApplication.java
+++ brahma-spring-boot-starter/src/main/java/com/brahma/BrahmaApplication.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource(
-        value = {"classpath:application.yml", "classpath:application-photo.yml"}
+        value = {"classpath:application.yml", "classpath:application-photo.yml", "classpath:application-wx.yml",
+                "classpath:application-security.yml"}
         , factory = BrahmaPropertySourceFactory.class
 )
 public class BrahmaApplication {
