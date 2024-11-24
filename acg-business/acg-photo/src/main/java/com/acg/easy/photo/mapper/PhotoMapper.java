a1b58fee4b7d5ae545afe787e95bf8c14b117aa3
875101222 <875101222@qq.com>
2024-11-24 23:29:53 +0800

    photo改名image

--- acg-business/acg-photo/src/main/java/com/acg/easy/photo/mapper/PhotoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.acg.easy.photo.mapper;
-
-import com.acg.easy.photo.entity.PhotoDo;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PhotoMapper extends BaseMapper<PhotoDo> {
-}
